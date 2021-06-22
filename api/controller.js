a3cc7b3bf1ab7818146db8544ea0bb937e6ba957
marcello zamora <zamoramaj@gmail.com>
2021-06-22 11:07:28 -0400

    completed the upload method

--- api/controller.js
+++ api/controller.js
@@ -1,6 +1,11 @@
 const controllers = {
     upload: (req, res) => {
-        console.log(req.file, req.body)
+        // console.log(req.file, req.body)
+        res.json({
+            name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+        })
     },
     notFound: (req, res) => {
         res.status(404)
